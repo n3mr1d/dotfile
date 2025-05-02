1cc3798989acf3a866fc06dc6a5f724c5d8a4acd
n3mr1d <nameraid@protonmail.com>
2025-05-02 08:06:49 +0700

    README.md update

---
+++ zsh_terminal/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+echo "📁 Backup your zshrc file to folder ~/backup"
+
+if [ ! -d "$HOME/backup" ]; then
+    echo "📂 Folder ~/backup belum ada. Membuat..."
+    mkdir -p "$HOME/backup"
+else
+    echo "✅ Folder ~/backup "
+fi
+
+# Backup file .zshrc
+cp "$HOME/.zshrc" "$HOME/backup/.zshrc.backup.$(date +%Y%m%d%H%M%S)"
+echo "✅ File .zshrc passed backup to ~/backup"
+cat ./zshrc > $HOME/.zshrc
